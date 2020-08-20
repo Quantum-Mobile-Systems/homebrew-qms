fcb1e224358ef1b531c74a370dbabb6c440eaf52
Ethan Carlson <ethancarlson00@gmail.com>
2020-08-20 15:27:04 -0700

    Revert "Revert "Initial Commit""
    This reverts commit ba26b9d88ce297ffdeb3b085271dd06d34399e07.

---
+++ Formula/libideviceactivation.rb
@@ -0,0 +1,45 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Libideviceactivation < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/ethanatwork/libideviceactivation/archive/master.zip"
+  version "1.1.2"
+  sha256 ""
+  # depends_on "cmake" => :build
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "libxml2"
+  depends_on "pkg-config" => :build
+  depends_on "libplist"
+  depends_on "libtasn1"
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./autogen.sh"
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          # As long as libplist builds without Cython
+                          # bindings, libimobiledevice must as well.
+                          "--without-cython",
+                          "--enable-debug-code"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test libideviceactivation`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
